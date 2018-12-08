23470ab5d535384f6dd70bb41091f1153779a0ea
obaramikako <quartersilver@gmail.com>
2018-12-08 14:06:44 +0900

    janコードを入力したら金額が返ってくるようにできた?

--- test_botto_price.py
+++ test_botto_price.py
@@ -2,7 +2,7 @@ import sqlite3
 import unittest
 
 
-def data(jan_code):
+def get_productname(jan_code):
     conn = sqlite3.connect("bottom_price.sqlite")
     cursor = conn.cursor()
 
@@ -17,9 +17,25 @@ def data(jan_code):
     return products
 
 
+def price(jan_code):
+    conn = sqlite3.connect("bottom_price.sqlite")
+    cursor = conn.cursor()
+
+    sql = "SELECT price FROM products where jan_code = ?;"
+    result = cursor.execute(sql, (jan_code,)).fetchone()
+    products = result[0]
+
+    conn.commit()
+    conn.close()
+    return products
+
+
 class TestBottomPrice(unittest.TestCase):
     def test_janコードを入力したら商品名が返ってくる(self):
-        self.assertEqual("おーいお茶ほうじ茶", data(4901085176146))
+        self.assertEqual("おーいお茶ほうじ茶", get_productname(4901085176146))
+
+    def test_janコードを入力したら金額が返ってくる(self):
+        self.assertEqual(129, price(4901085176146))
 
 
 if __name__ == "__main__":
